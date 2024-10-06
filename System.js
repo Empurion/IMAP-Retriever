fc7bc4eb9639f6328b07940e4b597a917d090ebd
Empurion <github@empurion.net>
2024-10-06 13:38:10 +0200

    Update System.js
    Removed unnecessary details

--- System.js
+++ System.js
@@ -8,26 +8,23 @@ module.exports = class System {
     }
     async build(){
 
-    }
-    async run(){
-
-        global.empurion = new Mailbox(
-            process.env[`MAILBOX_1_EMAIL`],
-            process.env[`MAILBOX_1_PASSWORD`], 
-            process.env[`MAILBOX_1_HOST`], 
-            process.env[`MAILBOX_1_PORT`]
-        )
-        global.dylan = new Mailbox(
+        global.mailboxOne = new Mailbox(
             process.env[`MAILBOX_0_EMAIL`], 
             process.env[`MAILBOX_0_PASSWORD`], 
             process.env[`MAILBOX_0_HOST`], 
             process.env[`MAILBOX_0_PORT`]
         )
-
-        await global.empurion.build()
-
-        await global.dylan.build()
-        await global.empurion.run()
-        await global.dylan.run()
+        global.mailboxTwo = new Mailbox(
+            process.env[`MAILBOX_1_EMAIL`],
+            process.env[`MAILBOX_1_PASSWORD`], 
+            process.env[`MAILBOX_1_HOST`], 
+            process.env[`MAILBOX_1_PORT`]
+        )
+        await global.mailboxOne.build()
+        await global.mailboxTwo.build()
+    }
+    async run(){
+        await global.mailboxOne.run()
+        await global.mailboxTwo.run()
     }
 }
